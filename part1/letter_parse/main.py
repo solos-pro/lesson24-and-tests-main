9fc926f8bd1ec2dc8720db9699e9f62d0c26fe0e
solos-pro <sergey-solonichenko@rambler.ru>
2022-05-01 13:54:11 +0300

    4. findall car number (ex.: в836иа51)

--- part1/letter_parse/main.py
+++ part1/letter_parse/main.py
@@ -13,12 +13,17 @@ import re
 letters = [
     "заявка от у444хн58 и ав333_78",
     "заявка на продажу от в836иа51",
+    "заявка"
 ]
 
 
 def get_plates(letters):
-    # TODO напишите ваш код здесь
-    pass
+
+    for item in letters:
+        for p in re.findall(r"([а-я]\d{3}[а-я]{2}\d{2,3})", item):
+            yield p
+        for p in re.findall(r"[а-я]{2}\d{3}_\d{2,3}", item):
+            yield p
 
 
 if __name__ == "__main__":
